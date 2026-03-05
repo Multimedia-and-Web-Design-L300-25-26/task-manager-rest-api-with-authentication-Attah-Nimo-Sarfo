4c0d10ad5538b53e4d7ee219da95ab7c885592af
Attah-Nimoh Sarfo <cobbydabbs409@gmail.com>
2026-03-05 17:35:32 +0000

    Initial commit – project files excluding .env

--- src/models/User.js
+++ src/models/User.js
@@ -11,9 +11,15 @@ import mongoose from "mongoose";
 
 
 const userSchema = new mongoose.Schema({
-  // Students implement
+ email: {
+  type: String,
+  required: true,
+  unique: true
+ },
+ password: {
+  type: String,
+  required: true
+ }
 });
 
-const User = mongoose.model("User", userSchema);
-
-export default User;
+export default mongoose.model("User", userSchema);
